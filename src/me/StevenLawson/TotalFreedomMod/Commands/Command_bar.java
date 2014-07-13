1a022e3cb74d863bc837e82874f0278174b31727
Liam <overhypedlord@gmail.com>
2014-07-13 09:02:34 +0100

    Fixed my stupid errors

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_bar.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_bar.java
@@ -1,6 +1,7 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
 import me.confuser.barapi.BarAPI;
 import net.minecraft.util.org.apache.commons.lang3.ArrayUtils;
 import net.minecraft.util.org.apache.commons.lang3.StringUtils;
@@ -9,6 +10,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 /*
   When compilng please use http://dev.bukkit.org/bukkit-plugins/bar-api/files/5-bar-api-v3-1/
+ else it won't work c:
 */
 
 
@@ -20,11 +22,11 @@ public class Command_bar extends TFM_Command
   When compilng please use http://dev.bukkit.org/bukkit-plugins/bar-api/files/5-bar-api-v3-1/
   Credit to PieGuy
  */
-  
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-    if (!TFM_Util.DEVELOPERS.contains(sender.getName()) {
+    if (!TFM_Util.DEVELOPERS.contains(sender.getName())) 
+    {
         playerMsg(TotalFreedomMod.MSG_NO_PERMS);
         return true;
     }
