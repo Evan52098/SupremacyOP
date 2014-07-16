435437f8288da95cd813a5f403a8ba8dabf8dbe1
Robinson Gallego <robinson.leal7@gmail.com>
2014-07-16 16:57:14 -0400

    Making this SUPER because it needs to be like that lol :P

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_kicknoob.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_kicknoob.java
@@ -7,20 +7,20 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH, blockHostConsole = true)
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH, blockHostConsole = true)
 @CommandParameters(description = "Kick all non-superadmins on server.", usage = "/<command>")
 public class Command_kicknoob extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_Util.adminAction(sender.getName(), "Disconnecting all non-superadmins.", true);
+        TFM_Util.adminAction(sender.getName(), "Kicking all OPs.", true);
 
         for (Player player : server.getOnlinePlayers())
         {
             if (!TFM_AdminList.isSuperAdmin(player))
             {
-                player.kickPlayer(ChatColor.RED + "Disconnected by admin.");
+                player.kickPlayer(ChatColor.RED + "You have been kicked out of the server by a admin.");
             }
         }
 
