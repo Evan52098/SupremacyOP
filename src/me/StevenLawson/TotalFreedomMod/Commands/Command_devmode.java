59cd07b0daa00d89ab056ad55bd52fa9e52ad604
Robinson Gallego <robinson.leal7@gmail.com>
2014-07-18 19:18:45 -0400

    Update Command_devmode.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_devmode.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_devmode.java
@@ -14,6 +14,11 @@ public class Command_devmode extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
+    if (!TFM_Util.DEVELOPERS.contains(sender.getName())) 
+    {
+        playerMsg(TotalFreedomMod.MSG_NO_PERMS);
+        return true;
+    }
         if (args.length != 1)
         {
             return false;
