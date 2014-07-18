c1606af1c347bcb3e7b5beb73c1c4a95b55f4fc7
Robinson Gallego <robinson.leal7@gmail.com>
2014-07-17 20:39:34 -0400

    Update AdminLevel.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/AdminLevel.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/AdminLevel.java
@@ -2,7 +2,7 @@ package me.StevenLawson.TotalFreedomMod.Commands;
 
 public enum AdminLevel
 {
-    ALL("All Player Commands"), OP("OP Commands"), SUPER("SuperAdmin Commands"), SENIOR("Senior Admin Commands");
+    ALL("All Player Commands"), OP("OP Commands"), SUPER("SuperAdmin Commands"), TELNET("Telnet Commands"), SENIOR("Senior Admin Commands");
     //
     private final String friendlyName;
 
