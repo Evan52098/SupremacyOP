58177fdc490f8f3af61b2d7683aed22c5a119128
Robinson Gallego <robinson.leal7@gmail.com>
2014-07-17 20:48:16 -0400

    Create Command_devchat.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_devchat.java
@@ -0,0 +1,39 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.TFM_PlayerData;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandParameters(
+        description = "Same as normal adminchat but for seniors.",
+        usage = "/<command> [message...]",
+        aliases = "devchat")
+public class Command_dev extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 0)
+        {
+            if (senderIsConsole)
+            {
+                playerMsg("Only in-game players can toggle Developer AdminChat.");
+                return true;
+            }
+
+            TFM_PlayerData userinfo = TFM_PlayerData.getPlayerData(sender_p);
+            userinfo.setAdminChat(!userinfo.inAdminChat());
+            playerMsg("Toggled Senior Admin Chat " + (userinfo.inAdminChat() ? "on" : "off") + ".");
+        }
+        else
+        {
+            TFM_Util.senioradminChatMessage(sender, StringUtils.join(args, " "), senderIsConsole);
+        }
+
+        return true;
+    }
+}
