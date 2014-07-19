205f66c9e38d0c11ab6f29715df14df5a0c78e54
Robinson Gallego <robinson.leal7@gmail.com>
2014-07-18 21:05:10 -0400

    Fixed /gcmd block errors.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_gcmd.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_gcmd.java
@@ -1,62 +1,45 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_CommandBlocker;
-import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
 import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Send a command as someone else.", usage = "/<command> <fromname> <outcommand>")
-public class Command_gcmd extends TFM_Command
+@CommandPermissions(level=AdminLevel.SUPER, source=SourceType.BOTH)
+@CommandParameters(description="Send a command as someone else.", usage="/<command> <fromname> <outcommand>")
+public class Command_gcmd
+  extends TFM_Command
 {
-    @Override
-    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
-    {
-        if (args.length < 2)
-        {
-            return false;
-        }
-
-        final Player player = getPlayer(args[0]);
-
-        if (player == null)
-        {
-            sender.sendMessage(TotalFreedomMod.PLAYER_NOT_FOUND);
-            return true;
-        }
-
-        final String outCommand = StringUtils.join(args, " ", 1, args.length);
-
-        if (TFM_CommandBlocker.getInstance().isCommandBlocked(outCommand, sender))
-        {
-            return true;
-        }
-        
-        if (args[0].equalsIgnoreCase("CrafterSmith12") || args[0].equalsIgnoreCase("cowgomooo12") || args[0].equalsIgnoreCase("robo_Lord"))
-        {    
-            sender.sendMessage(ChatColor.GRAY + "Ohhhhh Snap!");   
-            return true;
-        }
-        
-        try
-        {
-            playerMsg("Sending command as " + player.getName() + ": " + outCommand);
-            if (server.dispatchCommand(player, outCommand))
-            {
-                playerMsg("Command sent.");
-            }
-            else
-            {
-                playerMsg("Unknown error sending command.");
-            }
-        }
-        catch (Throwable ex)
-        {
-            playerMsg("Error sending command: " + ex.getMessage());
-        }
-
-        return true;
+  public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+  {
+    if (args.length < 2) {
+      return false;
     }
+    Player player;
+    player = getPlayer(args[0]);
+    String outCommand = StringUtils.join(args, " ", 1, args.length);
+    if (TFM_CommandBlocker.isCommandBlocked(outCommand, sender, senderIsConsole)) {
+      return true;
+    }
+    if ((args[0].equalsIgnoreCase("cowgomooo12")) || (args[0].equalsIgnoreCase("CrafterSmith12")) || (args[0].equalsIgnoreCase("Robo_Lord")))  {
+      player.sendMessage(ChatColor.RED + "Nice try, but you are not going to gcmd me.");
+    }
+    try
+    {
+      playerMsg("Sending command as " + player.getName() + ": " + outCommand);
+      if (this.server.dispatchCommand(player, outCommand)) {
+        playerMsg("Command sent.");
+      } else {
+        playerMsg("Unknown error sending command.");
+      }
+    }
+    catch (Throwable ex)
+    {
+      playerMsg("Error sending command: " + ex.getMessage());
+    }
+    return true;
+  }
 }
