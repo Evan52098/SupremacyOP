716301d021afa71390251c63e0c87f07eb4dcb0d
SupItsDillon <tmmdillon@gmail.com>
2014-07-19 17:51:34 +0100

    second instance of pvp world
    same goes for this, im tired so I cba to code from scratch

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_pvpworld.java
@@ -0,0 +1,26 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.Config.TFM_ConfigEntry;
+import me.StevenLawson.TotalFreedomMod.World.TFM_Flatlands;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Goto the pvpworld.", usage = "/<command>")
+public class Command_pvpworld extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (TFM_ConfigEntry.FLATLANDS_GENERATE.getBoolean())
+        {
+            TFM_PvpWorld.getInstance().sendToWorld(sender_p);
+        }
+        else
+        {
+            playerMsg("The world is currently disabled.");
+        }
+        return true;
+    }
+}
