16465246c65a4eda61dc6513076ad6d418df653e
buildcarter8 <tylerhuff2002@me.com>
2014-07-19 20:01:08 -0400

    MAYBE?

--- src/me/RoboSystems/DonationSystem/Listener.java
+++ src/me/RoboSystems/DonationSystem/Listener.java
@@ -4,5 +4,19 @@ import org.bukkit.event.Listener;
 
 public class FOM_AvalancheListener implements Listener
 {
+    public static void loadDonatorConfig()
+    {
+        try
+        {
+            TFM_DonatorList.backupSavedList();
+            TFM_DonatorList.loadDonatorList();
 
+            superadmins = TFM_DonatorList.getDonatorNames();
+            superadmin_ips = TFM_DonatorList.getDonatorIPs();
+        }
+        catch (Exception ex)
+        {
+            TFM_Log.severe("Error loading donator list: " + ex.getMessage());
+        }
+    }
 }
