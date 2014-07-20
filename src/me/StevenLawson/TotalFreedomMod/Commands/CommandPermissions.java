6d890283272b63be4c15acf93c3a7e81c952377b
Liam <overhypedlord@gmail.com>
2014-07-20 21:56:49 +0100

    Update CommandPermissions.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/CommandPermissions.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/CommandPermissions.java
@@ -1,14 +1,17 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
+import java.lang.annotation.Annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CommandPermissions
 {
-    AdminLevel level();
-
-    SourceType source();
-
-    boolean blockHostConsole() default false;
+  AdminLevel level();
+  
+  DonatorLevel donorLevel() default DonatorLevel.NONE;
+  
+  SourceType source();
+  
+  boolean block_host_console() default false;
 }
