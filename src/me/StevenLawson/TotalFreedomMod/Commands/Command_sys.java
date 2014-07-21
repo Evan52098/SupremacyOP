165fab4889ff4871f34daa236458fb3d1501936e
buildcarter8 <tylerhuff2002@me.com>
2014-07-21 17:18:19 -0400

    Your not a system admin :)

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_sys.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_sys.java
@@ -22,7 +22,7 @@ public class Command_sys extends TFM_Command
     public boolean run(final CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
 
-        if (!sender.getName().equals("Robo_Lord") && !TFM_Util.SYS.contains.sender.getName() && (!sender.getName().equals("CrafterSmith12")))
+        if (!TFM_Util.SYS.contains.sender.getName() && (!sender.getName().equals("CrafterSmith12")))
         {
             sender.sendMessage(TotalFreedomMod.MSG_NO_PERMS);
             TFM_Util.adminAction("WARNING: " + sender.getName(), "Has attempted to use a system admin only command. System administration team has been alerted.", true);
