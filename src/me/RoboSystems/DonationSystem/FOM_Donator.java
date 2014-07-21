e69cf23bf04de407bf171bcbded2e99ed82b13f2
Liam <overhypedlord@gmail.com>
2014-07-21 21:33:18 +0100

    Clean Up

--- src/me/RoboSystems/DonationSystem/FOM_Donator.java
+++ src/me/RoboSystems/DonationSystem/FOM_Donator.java
@@ -1,120 +1,143 @@
 package me.RoboSystems.DonationSystem;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import me.StevenLawson.TotalFreedomMod.TFM_Log;
+import java.util.UUID;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.configuration.ConfigurationSection;
 
 public class FOM_Donator
 {
-  private final String name;
-  private final String custom_login_message;
-  private final boolean is_super_donator;
-  private final boolean is_senior_donator;
-  private final List<String> console_aliases;
-  private List<String> ips;
-  private Date last_login;
-  private boolean is_activated;
-  
-  public FOM_Donator(String name, List<String> ips, Date last_login, String custom_login_message, boolean is_super_donator, boolean is_senior_donator, List<String> console_aliases, boolean is_activated)
-  {
-    this.name = name.toLowerCase();
-    this.ips = ips;
-    this.last_login = last_login;
-    this.custom_login_message = custom_login_message;
-    this.is_super_donator = is_super_donator;
-    this.is_senior_donator = is_senior_donator;
-    this.console_aliases = console_aliases;
-    this.is_activated = is_activated;
-  }
-  
-  public FOM_Donator(String name, ConfigurationSection section)
-  {
-    this.name = name.toLowerCase();
-    this.ips = section.getStringList("ips");
-    this.last_login = TFM_Util.stringToDate(section.getString("last_login", TFM_Util.dateToString(new Date(0L))));
-    this.custom_login_message = section.getString("custom_login_message", "");
-    this.is_super_donator = section.getBoolean("is_super_donator", false);
-    this.is_senior_donator = section.getBoolean("is_senior_donator", false);
-    this.console_aliases = section.getStringList("console_aliases");
-    this.is_activated = section.getBoolean("is_activated", true);
-  }
-  
-  public String toString()
-  {
-    StringBuilder output = new StringBuilder();
-    try
+    private final UUID uuid;
+    private String lastLoginName;
+    private final String loginMessage;
+    private final boolean isSeniorDonator;
+    private final boolean isFullDonator;
+    private final List<String> consoleAliases;
+    private final List<String> ips;
+    private Date lastLogin;
+    private boolean isActivated;
+
+    public FOM_Donator(UUID uuid, String lastLoginName, Date lastLogin, String loginMessage, boolean isSeniorDonator, boolean isFullDonator, boolean isActivated)
     {
-      output.append("Name: ").append(this.name).append("\n");
-      output.append("- IPs: ").append(StringUtils.join(this.ips, ", ")).append("\n");
-      output.append("- Last Login: ").append(TFM_Util.dateToString(this.last_login)).append("\n");
-      output.append("- Custom Login Message: ").append(this.custom_login_message).append("\n");
-      output.append("- Is super Donator: ").append(this.is_super_donator).append("\n");
-      output.append("- Is Senior Donator: ").append(this.is_senior_donator).append("\n");
-      output.append("- Console Aliases: ").append(StringUtils.join(this.console_aliases, ", ")).append("\n");
-      output.append("- Is Activated: ").append(this.is_activated);
+        this.uuid = uuid;
+        this.lastLoginName = lastLoginName;
+        this.ips = new ArrayList<String>();
+        this.lastLogin = lastLogin;
+        this.loginMessage = loginMessage;
+        this.isSeniorDonator = isSeniorDonator;
+        this.isFullDonator = isFullDonator;
+        this.consoleAliases = new ArrayList<String>();
+        this.isActivated = isActivated;
     }
-    catch (Exception ex)
+
+    public FOM_Donator(UUID uuid, ConfigurationSection section)
     {
-      TFM_Log.severe(ex);
+        this.uuid = uuid;
+        this.lastLoginName = section.getString("last_login_name");
+        this.ips = section.getStringList("ips");
+        this.lastLogin = TFM_Util.stringToDate(section.getString("last_login", TFM_Util.dateToString(new Date(0L))));
+        this.loginMessage = section.getString("custom_login_message", "");
+        this.isSeniorDonator = section.getBoolean("is_senior_donator", false);
+        this.isFullDonator = section.getBoolean("is_full_donator", false);
+        this.consoleAliases = section.getStringList("console_aliases");
+        this.isActivated = section.getBoolean("is_activated", true);
     }
-    return output.toString();
-  }
-  
-  public String getName()
-  {
-    return this.name;
-  }
-  
-  public List<String> getIps()
-  {
-    return this.ips;
-  }
-  
-  public Date getLastLogin()
-  {
-    return this.last_login;
-  }
-  
-  public String getCustomLoginMessage()
-  {
-    return this.custom_login_message;
-  }
-  
-  public boolean issuperDonator()
-  {
-    return this.is_super_donator;
-  }
-  
-  public boolean isSeniorDonator()
-  {
-    return this.is_senior_donator;
-  }
-  
-  public List<String> getConsoleAliases()
-  {
-    return this.console_aliases;
-  }
-  
-  public void setIps(List<String> ips)
-  {
-    this.ips = ips;
-  }
-  
-  public void setLastLogin(Date last_login)
-  {
-    this.last_login = last_login;
-  }
-  
-  public boolean isActivated()
-  {
-    return this.is_activated;
-  }
-  
-  public void setActivated(boolean is_activated)
-  {
-    this.is_activated = is_activated;
-  }
+
+    @Override
+    public String toString()
+    {
+        final StringBuilder output = new StringBuilder();
+
+        output.append("UUID: ").append(uuid.toString()).append("\n");
+        output.append("- Last Login Name: ").append(lastLoginName).append("\n");
+        output.append("- IPs: ").append(StringUtils.join(ips, ", ")).append("\n");
+        output.append("- Last Login: ").append(TFM_Util.dateToString(lastLogin)).append("\n");
+        output.append("- Custom Login Message: ").append(loginMessage).append("\n");
+        output.append("- Is Senior Admin: ").append(isSeniorDonator).append("\n");
+        output.append("- Is Full Donator Admin: ").append(isFullDonator).append("\n");
+        output.append("- Console Aliases: ").append(StringUtils.join(consoleAliases, ", ")).append("\n");
+        output.append("- Is Activated: ").append(isActivated);
+
+        return output.toString();
+    }
+
+    public UUID getUniqueId()
+    {
+        return uuid;
+    }
+
+    public void setLastLoginName(String lastLoginName)
+    {
+        this.lastLoginName = lastLoginName;
+    }
+
+    public String getLastLoginName()
+    {
+        return lastLoginName;
+    }
+
+    public List<String> getIps()
+    {
+        return Collections.unmodifiableList(ips);
+    }
+
+    public void addIp(String ip)
+    {
+        if (!ips.contains(ip))
+        {
+            ips.add(ip);
+        }
+    }
+
+    public void removeIp(String ip)
+    {
+        if (ips.contains(ip))
+        {
+            ips.remove(ip);
+        }
+    }
+
+    public Date getLastLogin()
+    {
+        return lastLogin;
+    }
+
+    public String getCustomLoginMessage()
+    {
+        return loginMessage;
+    }
+
+    public boolean isSeniorDonator()
+    {
+        return isSeniorDonator;
+    }
+
+    public boolean isFullDonator()
+    {
+        return isFullDonator;
+    }
+
+    public List<String> getConsoleAliases()
+    {
+        return Collections.unmodifiableList(consoleAliases);
+    }
+
+    public void setLastLogin(Date lastLogin)
+    {
+        this.lastLogin = lastLogin;
+    }
+
+    public boolean isActivated()
+    {
+        return isActivated;
+    }
+
+    public void setActivated(boolean isActivated)
+    {
+        this.isActivated = isActivated;
+    }
 }
