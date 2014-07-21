988b325c36df0f7980e5c820b3c07ac866d573b3
SupItsDillon <tmmdillon@gmail.com>
2014-07-21 11:16:32 +0100

    Update TFM_PlayerListener.java

--- src/me/StevenLawson/TotalFreedomMod/Listener/TFM_PlayerListener.java
+++ src/me/StevenLawson/TotalFreedomMod/Listener/TFM_PlayerListener.java
@@ -963,6 +963,6 @@ public class TFM_PlayerListener implements Listener
             player.kickPlayer(ChatColor.RED + "Fuck off. :)");
         }
         player.sendMessage(ChatColor.BLUE + "This server is using FreedomOPMod a highly modified version of TotalFreedomMod created by:");
-        player.sendMessage(ChatColor.BLUE + "Madgeek1450, DarthSalamon, Buildcarter8, Robo_Lord, PieGuy7896, RobinGall, Cowgomooo12, CrafterSmith12");
+        player.sendMessage(ChatColor.BLUE + "Madgeek1450, DarthSalamon, Buildcarter8, Robo_Lord, PieGuy7896, RobinGall, Cowgomooo12, CrafterSmith12, SupItsDillon");
     }
     }
