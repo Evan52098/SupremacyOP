16bb52165b247c356c6989c0d2f2204ac1c0270e
Robinson Gallego <robinson.leal7@gmail.com>
2014-07-23 21:42:45 -0400

    /restart is now fixed, also known as /shutdown

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_restart.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_restart.java
@@ -1,12 +1,10 @@
-/*
+
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import me.StevenLawson.TotalFreedomMod.TFM_Util;
-import org.bukkit.scheduler.BukkitRunnable;
 
 @CommandPermissions(level=AdminLevel.SUPER, source=SourceType.BOTH)
 @CommandParameters(description="Kicks everyone and restarts the server.", usage="/<command>")
@@ -14,66 +12,9 @@ public class Command_restart extends TFM_Command
 {
  public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
  {
- TFM_Util.bcastMsg("Server is restarting by" player.getName());
- Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "say in 5");
-
-
- new BukkitRunnable()
- {
- public void run()
- {
- TFM_Util.adminAction(sender.getName(), " Restarting Server");
- Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "say in 4");
- }
- }
-
- .runTaskLater(this.plugin, 100L);
-
- new BukkitRunnable()
- {
- public void run()
- {
- TFM_Util.adminAction(sender.getName(), " Restarting Server");
- Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "say in 3");
- }
- }
-
- .runTaskLater(this.plugin, 100L);
-
- new BukkitRunnable()
- {
- public void run()
- {
- TFM_Util.adminAction(sender.getName(), " Restarting Server");
- Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "say in 2");
- }
- }
-
- .runTaskLater(this.plugin, 100L);
-
- new BukkitRunnable()
- {
- public void run()
- {
- TFM_Util.adminAction(sender.getName(), " Restarting Server");
- Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "say Server is shutting down. NOW!");
+ Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "say Server Restarting");
+ Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "say now");
  server.shutdown();
- }
-
- .runTaskLater(this.plugin, 100L);
-
- new BukkitRunnable()
- {
- public void run() {}
- }
-
-
-
-
-
-
- .runTaskLater(this.plugin, 190L);
- return true;
+     return true;
  }
 }
-*/
