274962747ea3142cada78c16e0021bd6a76fcf0e
Tyler <tylerhuff2002@me.com>
2014-07-25 18:30:41 -0400

    Update Command_ai.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_ai.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_ai.java
@@ -14,13 +14,15 @@ public class Command_ai extends TFM_Command
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
         
-            playerMsg(ChatColor.AQUA + "The following is accurate as of 10/07/14");
-            playerMsg(ChatColor.GREEN + "To apply for admin you need to go to the forums at http://freedomop.boards.net");
+            playerMsg(ChatColor.AQUA + "The following is accurate as of 7/25/14");
+            playerMsg(ChatColor.RED + "Currently applying for Super Admin is impossible");
+         /*  playerMsg(ChatColor.GREEN + "To apply for admin you need to go to the forums at http://freedomop.boards.net");
             playerMsg(ChatColor.YELLOW + "Then read the requirements in the 'admin applications' board.");
             playerMsg(ChatColor.WHITE + "Then if you feel you are ready, make a new thread in the 'admin applications'' board.");
             playerMsg(ChatColor.BLUE + "And fill out the template in the new thread.");
             playerMsg(ChatColor.RED + "We ask for you not to ask existing admins for recommendations, this will get your application denied.");
             playerMsg(ChatColor.GOLD + "Good Luck!");
+        */
             return true;
     }
 }
