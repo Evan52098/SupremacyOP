35ae87e79a32e09a13f0e15d0d7091279808c321
Robinson Gallego <robinson.leal7@gmail.com>
2014-07-25 20:56:22 -0400

    fix

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_sexy.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_sexy.java
@@ -1,6 +1,5 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -14,7 +13,7 @@ public class Command_sexy extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        Bukkit.dispatchCommand(sender, "me &ebuildcarter8 is sexy!");
+        sender_p.chat("buildcarter8 is totally sexy!");
         playerMsg("You just said buildcarter8 is sexy! :O", ChatColor.GRAY);
         playerMsg("Also buildcarter8 is sexy :) and he's our lead dev!", ChatColor.GOLD);
         return true;  
